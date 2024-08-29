f97fc16a0c01a934fe153df8ea7f6bc0453fd10c
Yani Rahmawati <107221750+yanirahmawati234@users.noreply.github.com>
2024-08-29 13:27:30 +0700

    Delete Soal2.java

--- Soal2.java
+++
@@ -1,7 +0,0 @@
-public class Soal2 { //pembuatan class bernama Soal2
-    public static void main(String[] args) {
-        int i = 42; //variabel i bertipe integer
-        String s = (i < 40) ? "life" : (i > 50) ? "universe" : "everything"; //variabel s menyimpan hasil operasi percabangan if-else
-        System.out.println(s); //Mencetak variabel s
-    }
-}
