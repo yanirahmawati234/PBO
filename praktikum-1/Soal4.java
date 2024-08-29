f92ed3465e86864017fc86a8e0ce14825885d60f
Yani Rahmawati <107221750+yanirahmawati234@users.noreply.github.com>
2024-08-29 13:25:58 +0700

    Add files via upload

---
+++ praktikum-1/Soal4.java
@@ -0,0 +1,9 @@
+public class Soal4 { //pembuatan class bernama Soal4
+    public static void main(String[] args) {
+        byte angka1 = 125; //variabel angka1 bertipe byte
+        byte angka2 = 6; //variabel angka2 bertipe byte
+        byte hasil = (byte) (angka1+angka2); //variabel hasil bertipe byte yang menyimpan hasil penjumlahan variabel angka1 dan angka2
+
+        System.out.println("Hasil 1 "+hasil); //mencetak String Hasil 1 dengan hasil penjumlahan pada variabel hasil
+    }
+}
